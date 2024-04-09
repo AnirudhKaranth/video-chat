66dc7e0b490f1905e59dcbf365a67bd1559dc67d
dskarkera23 <dskarkera23@gmail.com>
2024-04-10 00:13:05 +0530

    frontend-improved2.0

--- src/components/JoinComponent.tsx
+++ src/components/JoinComponent.tsx
@@ -3,7 +3,7 @@ import React, { FormEvent, useState } from "react";
 import Navbar from "./Navbar";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
+import Image from 'next/image';
 const JoinComponent = () => {
   const [roomId, setRoomId] = useState<string>("");
   const router = useRouter();
@@ -21,21 +21,27 @@ const JoinComponent = () => {
   return (
     <div className="h-screen w-full bg-gray-900  ">
       <Navbar handleLogout={handleLogout} />
-      <div className=" w-wull pt-48 flex h-full items-start  justify-center">
+      <Image
+      src="/bgimg6.svg"
+      width={100}
+      height={100}
+      className="absolute inset-0 w-full h-full object-cover z-0 "
+      alt="Background Image"
+    />
+      <div className=" w-wull pt-48 flex h-full items-start  justify-center z-10">
         <form
           onSubmit={handleJoinRoom}
-          className="flex h-56 w-96 flex-col items-center justify-center gap-2 bg-gray-800 rounded-md border-2 border-gray-500 p-3 shadow-md"
-        >
+          className="flex flex-col items-center justify-center gap-4  bg-gray-800 bg-opacity-50 rounded-3xl border-2 border-gray-400 p-10 shadow-lg z-10">
           <input
             type="text"
             placeholder="Enter the meeting ID"
             value={roomId}
             onChange={(e) => setRoomId(e.target.value)}
-            className="border-2 border-gray-200 p-2 rounded-md"
+            className="border-2 border-gray-200 px-10 py-3 rounded-xl z-10"
           />
           <button
             type="submit"
-            className="rounded-sm bg-blue-300 px-6 py-2 text-black hover:bg-blue-500"
+            className="text-white mt-4 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-700/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-xl text-sm px-5 py-2.5 text-center me-2  "
           >
             Join
           </button>
--- src/components/videocall/Prejoin.tsx
+++ src/components/videocall/Prejoin.tsx
@@ -5,6 +5,7 @@ import { userType } from './Room'
 import Navbar from '../Navbar'
 import { signOut } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image';
 
 const Prejoin = ({roomId, user, handlePrejoinValues}:{roomId:string, user:userType,  handlePrejoinValues: (choice:LocalUserChoices) => void }) => {
   let userName = user.name || "userName"
@@ -16,27 +17,34 @@ const Prejoin = ({roomId, user, handlePrejoinValues}:{roomId:string, user:userTy
   return (
     <>
     <Navbar handleLogout={handleLogout}/>
-    <div style={{ display: 'grid', placeItems: 'center', height: '100%' }}>
-        Joining Room: {roomId}
-        <PreJoin 
-          className="overflow-visible max-w-96 max-h-96"
-          onError={(err) =>
-            console.log("Error while setting up prejoin", err)
-          }
-          defaults={{
-            username: userName,
-            videoEnabled: true,
-            audioEnabled: true,
-          }}
-          onSubmit={(values) => {
-            console.log("Joining with: ", values);
-            handlePrejoinValues(values)
-            
-          }}
-          >
-
-        </PreJoin>
+    <Image
+      src="/bgimg12.svg"
+      width={100}
+      height={100}
+      className="absolute inset-0 w-full h-full object-cover z-0 "
+      alt="Background Image"
+    />
+    <div className="z-10" style={{ display: 'grid', placeItems: 'center', height: '100%' }}>
+  <div className="bg-gray-800 bg-opacity-35 rounded-xl p-6 shadow-lg z-10">
+    <h4 className="text-xl font-semibold text-white mb-4 z-10">Joining Room: {roomId}</h4>
+    <PreJoin 
+      className="overflow-visible max-w-96 max-h-96 z-10"
+      onError={(err) =>
+        console.log("Error while setting up prejoin", err)
+      }
+      defaults={{
+        username: userName,
+        videoEnabled: true,
+        audioEnabled: true,
+      }}
+      onSubmit={(values) => {
+        console.log("Joining with: ", values);
+        handlePrejoinValues(values)
+      }}
+    />
   </div>
+</div>
+
           </>
   )
 }
