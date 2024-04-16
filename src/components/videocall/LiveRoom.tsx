115cd7aeb9ab0090fb721f1646f09ab4a1ddd18f
AnirudhKaranth <anikaranth47@gmail.com>
2024-04-16 11:30:44 +0530

    loaded the model

--- src/components/videocall/LiveRoom.tsx
+++ src/components/videocall/LiveRoom.tsx
@@ -15,6 +15,8 @@ import "@livekit/components-styles";
 import { Holistic } from "@mediapipe/holistic";
 import { Camera } from "@mediapipe/camera_utils";
 import Webcam from "react-webcam";
+import * as tf from '@tensorflow/tfjs';
+
 
 type LiveRoomType = {
   roomId: string;
@@ -24,29 +26,46 @@ type LiveRoomType = {
 };
 
 const LiveRoom = ({ roomId, userChoices, OnDisconnected }: LiveRoomType) => {
+  let sequence:any = [];
+  let predictions:any = [];
   const webcamRef = useRef<Webcam>(null);
   const [isSignLanguageEnabled, setIsSignLanguageEnabled] = useState(false);
   const [signLangActive, setsignLangActive] = useState(false);
-
-
+  
+  
   const { data } = api.room.joinRoom.useQuery({ roomId: roomId });
-
-  // const roomOptions = useMemo((): RoomOptions => {
-  //   return {
-  //     videoCaptureDefaults: {
-  //       deviceId: userChoices?.videoDeviceId ?? undefined,
-  //     },
-
-  //     audioCaptureDefaults: {
-  //       deviceId: userChoices?.audioDeviceId ?? undefined,
-  //     },
-  //     adaptiveStream: { pixelDensity: "screen" },
-  //     dynacast: true,
-  //   };
-  // }, [userChoices]);
-
   
   
+  function extractKeypoints(results:any) {
+    let lh = [];
+    let rh = [];
+    
+    // Extract left hand keypoints
+    if (results.left_hand_landmarks) {
+      lh = results.leftHandLandmarks.map((res:any) => [res.x, res.y, res.z]).flat();
+    } else {
+      lh = Array(21*3).fill(0);
+    }
+    
+    // Extract right hand keypoints
+    if (results.right_hand_landmarks) {
+      rh = results.rightHandLandmarks.map((res:any) => [res.x, res.y, res.z]).flat();
+    } else {
+      rh = Array(21*3).fill(0);
+    }
+    
+    // Concatenate left hand and right hand keypoints
+    return lh.concat(rh);
+  }
+  const mode=async() => {
+    const model = await tf.loadLayersModel("https://cloud-object-storage-cos-standard-ufe.s3.jp-tok.cloud-object-storage.appdomain.cloud/model.json");
+    console.log(model)
+    
+    
+  }
+
+
+  /*
   useEffect(() => {
     const holistic:any = new Holistic({
       locateFile: (file: string) => {
@@ -55,9 +74,18 @@ const LiveRoom = ({ roomId, userChoices, OnDisconnected }: LiveRoomType) => {
     });
 
   holistic.onResults((res: any) => {
-    console.log(res)
+    let keypoints = extractKeypoints(res);
+    sequence.push(keypoints);
+    sequence = sequence.slice(-15);
     // leftHandLandmarks
     //rightHandLandmarks
+
+    if (sequence.length === 30) {
+      let sequenceInput = [sequence]; // Convert sequence to a 3D array
+      let res = model.predict(sequenceInput)[0];
+      // console.log(actions[res.indexOf(Math.max(...res))]); // Assuming `actions` is an array
+      predictions.push(res.indexOf(Math.max(...res)));
+  }
   })
 
     if (
@@ -81,7 +109,7 @@ const LiveRoom = ({ roomId, userChoices, OnDisconnected }: LiveRoomType) => {
       },10000)
       return()=>clearInterval(interval);
     }, [webcamRef.current])
-
+*/
 
 
   // const signLanguage = (enable: boolean) => {
@@ -138,7 +166,7 @@ const LiveRoom = ({ roomId, userChoices, OnDisconnected }: LiveRoomType) => {
         SettingsComponent={SettingsMenu}
       />
       <button type="button"  className=" !absolute !left-72 bottom-3 lk-button z-2" onClick={() => {
-        setIsSignLanguageEnabled(!isSignLanguageEnabled)
+        mode()
         // signLanguage(isSignLanguageEnabled)
         }}>{isSignLanguageEnabled?"Diable Sign Lang":"Enable Sign Lang"}</button>
     </LiveKitRoom>
